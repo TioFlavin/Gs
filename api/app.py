b693aec5c284213d9cef461df210f52981b4c24c
FlixGo <92895161+TioFlavin@users.noreply.github.com>
2024-09-12 06:54:48 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -14,7 +14,7 @@ bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
 # Configuração do Webhook
-@app.route('/' + TOKEN, methods=['POST'])
+@app.route('/api/' + TOKEN, methods=['POST'])
 def webhook():
     json_str = request.get_data().decode('UTF-8')
     update = telebot.types.Update.de_json(json_str)
